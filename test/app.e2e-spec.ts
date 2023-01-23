5596f69bb1525a764726672435a53e7ffa5ef175
Damian Ziemba <nazriel@dzfl.pl>
2023-01-23 14:48:47 +0100

    style: change eslint and pretter config for sane defaults + reformat code.
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,16 +1,16 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import { AppModule } from "./../src/app.module";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe("AppController (e2e)", () => {
+    let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 });
