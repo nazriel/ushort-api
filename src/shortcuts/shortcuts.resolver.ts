a6cd69c88aa1c3400138fd6ddf57f2775670e2a5
Damian Ziemba <nazriel@dzfl.pl>
2023-01-24 22:46:12 +0100

    feat: implement findOneByHash and add required decorators for shortcuts GQL resolver
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- src/shortcuts/shortcuts.resolver.ts
+++ src/shortcuts/shortcuts.resolver.ts
@@ -1,11 +1,18 @@
 import { NotFoundException } from "@nestjs/common";
+import { Resolver, Query, Args } from "@nestjs/graphql";
 import { Shortcut } from "./entities/shortcut.entity";
 import { ShortcutsService } from "./shortcuts.service";
 
+@Resolver(() => Shortcut)
 export class ShortcutsResolver {
     constructor(private readonly shortcutsService: ShortcutsService) {}
 
-    findOneByHash(hash: string): Shortcut | null {
-        return new Shortcut({ hash });
+    @Query(() => Shortcut, { name: "shortcut" })
+    findOneByHash(@Args("hash") hash: string): Shortcut | null {
+        const record = this.shortcutsService.findOneByHash(hash);
+        if (!record) {
+            throw new NotFoundException("shortcut not found");
+        }
+        return record;
     }
 }
