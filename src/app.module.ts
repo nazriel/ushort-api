b101a4c95d8b461f3fba8c4891f35f401c933a33
Damian Ziemba <nazriel@dzfl.pl>
2023-01-24 22:34:23 +0100

    feat: wire up graphql
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
 import { ShortcutsModule } from "./shortcuts/shortcuts.module";
 
 @Module({
-    imports: [ShortcutsModule],
+    imports: [
+        ShortcutsModule,
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: "schema.gql",
+            debug: true,
+            playground: true,
+        }),
+    ],
     controllers: [],
     providers: [],
 })
