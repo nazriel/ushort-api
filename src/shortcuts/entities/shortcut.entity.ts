7c6d0aac294c3ddb6ab2c80b74210ff465578d83
Damian Ziemba <nazriel@dzfl.pl>
2023-01-24 22:50:44 +0100

    feat: added GQL related decorators to shortcut entity
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- src/shortcuts/entities/shortcut.entity.ts
+++ src/shortcuts/entities/shortcut.entity.ts
@@ -1,8 +1,16 @@
 /* istanbul ignore file */
 
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+
+@ObjectType({ description: "shortcut " })
 export class Shortcut {
+    @Field(() => ID)
     hash: string;
+
+    @Field()
     url: string;
+
+    @Field()
     visits: number;
 
     constructor(partial: Partial<Shortcut>) {
