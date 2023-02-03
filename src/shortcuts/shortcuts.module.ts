f25c3c38dfee874da902ea161dbd5f67f8987241
Damian Ziemba <nazriel@dzfl.pl>
2023-02-03 01:09:56 +0100

    bug: use relative import for prisma.service inside shortcuts module
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- src/shortcuts/shortcuts.module.ts
+++ src/shortcuts/shortcuts.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "src/prisma.service";
+import { PrismaService } from "../prisma.service";
 import { ShortcutsController } from "./controllers/shortcuts.controller";
 import { ShortcutsResolver } from "./shortcuts.resolver";
 import { ShortcutsService } from "./shortcuts.service";
